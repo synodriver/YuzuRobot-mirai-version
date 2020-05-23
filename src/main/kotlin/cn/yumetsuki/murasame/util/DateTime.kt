4c3cb2edf94b92cea95a19f2663f75d237a6bd11
yumetsuki <540160575@qq.com>
2020-05-24 07:45:37 +0800

    迁移DateTime Util

---
+++ src/main/kotlin/cn/yumetsuki/murasame/util/DateTime.kt
@@ -0,0 +1,32 @@
+package cn.yumetsuki.murasame.util
+
+import java.time.*
+
+fun LocalDateTime.timeStamp(): Long {
+    return toInstant(ZoneOffset.of("+8")).toEpochMilli()
+}
+
+fun Long.fromTimeStamp(): LocalDateTime {
+    return LocalDateTime.ofInstant(
+        Instant.ofEpochMilli(this),
+        ZoneId.systemDefault()
+    )
+}
+
+fun LocalTime.simpleFormat(): String {
+    return listOf(
+        hour.padZero(), minute.padZero(), second.padZero()
+    ).joinToString(":")
+}
+
+fun LocalDateTime.simpleFormat(): String {
+    return listOf(
+        year, monthValue.padZero(), dayOfMonth.padZero()
+    ).joinToString("-") + " " + listOf(
+        hour.padZero(), minute.padZero(), second.padZero()
+    ).joinToString(":")
+}
+
+private fun Int.padZero(): String {
+    return if (this < 10) return "0$this" else "$this"
+}
