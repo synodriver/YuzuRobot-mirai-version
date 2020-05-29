9ab257042f8b7c087b7a5096ba7cd754248c2bcb
yumetsuki <540160575@qq.com>
2020-05-29 11:33:24 +0800

    给文件判断上锁

--- src/main/kotlin/cn/yumetsuki/util/NetImage.kt
+++ src/main/kotlin/cn/yumetsuki/util/NetImage.kt
@@ -7,23 +7,29 @@ import io.ktor.client.request.get
 import io.ktor.client.request.request
 import io.ktor.client.statement.HttpResponse
 import io.ktor.utils.io.jvm.javaio.toInputStream
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import java.io.File
 import java.io.IOException
 import java.io.InputStream
 
+private val mutex = Mutex()
+
 @Suppress("BlockingMethodInNonBlockingContext", "EXPERIMENTAL_API_USAGE")
 suspend fun getNetImageStreamFromUrl(url: String, cache: Boolean = true, cachePath: String = "static") : InputStream {
     return if (cache) {
-        val dic = File(cachePath)
-        if (!dic.exists()) dic.mkdir()
-        val filePath = "$cachePath/${url.substring(url.lastIndexOf("/") + 1)}"
-        var file = File(filePath)
-        if (!file.exists()) {
-            file.createNewFile()
-            file = File(filePath)
-            file.writeBytes(HttpClient(CIO).get<HttpResponse>(url).content.toInputStream().readAllBytes())
+        mutex.withLock {
+            val dic = File(cachePath)
+            if (!dic.exists()) dic.mkdir()
+            val filePath = "$cachePath/${url.substring(url.lastIndexOf("/") + 1)}"
+            var file = File(filePath)
+            if (!file.exists()) {
+                file.createNewFile()
+                file = File(filePath)
+                file.writeBytes(HttpClient(CIO).get<HttpResponse>(url).content.toInputStream().readAllBytes())
+            }
+            file.inputStream()
         }
-        file.inputStream()
     } else {
         HttpClient(CIO).get<HttpResponse>(url).content.toInputStream()
     }
