5bd4907bca2878d62c980f449ab62dca8aec42d0
yumetsuki <540160575@qq.com>
2020-05-29 15:30:40 +0800

    修复了at仍然不会说话的bug

--- src/main/kotlin/cn/yumetsuki/murasame/feature/common/matchChatterBotFeature.kt
+++ src/main/kotlin/cn/yumetsuki/murasame/feature/common/matchChatterBotFeature.kt
@@ -12,6 +12,7 @@ import io.ktor.client.engine.cio.CIO
 import io.ktor.client.request.header
 import io.ktor.client.request.post
 import net.mamoe.mirai.event.GroupMessageSubscribersBuilder
+import net.mamoe.mirai.message.GroupMessageEvent
 import net.mamoe.mirai.message.data.PlainText
 import java.io.File
 
@@ -20,9 +21,23 @@ fun GroupMessageSubscribersBuilder.matchChatterBot() {
 
     val atStatusDao : AtStatusDao by globalKoin().inject()
 
+    suspend fun GroupMessageEvent.replyRandom() {
+        atStatusDao.findAtStatusByUserIdAndGroupIdAsync(sender.id, group.id).await().also { status ->
+            val replies = arrayOf(
+                getNetImageStreamFromUrl(CALL_BOT_1).uploadAsImage() + "叫吾辈有什么事吗？主人～",
+                getNetImageStreamFromUrl(CALL_BOT_2).uploadAsImage() + "诶？抱歉主人，刚才在稍微想点事情，叫吾辈有什么事吗？",
+                getNetImageStreamFromUrl(CALL_BOT_3).uploadAsImage() + "唔姆？？"
+            )
+            reply(replies.random())
+            atStatusDao.addOrUpdateAtStatusAsync(status.copy(atCount = status.atCount + 1)).await()
+        }
+    }
+
     atBot {
         recordReplyEvent()
-        val request = message[PlainText]?.content?.trim()?.removePunctuation()?:""
+        val request = message[PlainText]?.content?.trim()?.removePunctuation()?.takeIf { it.isNotEmpty() }?:replyRandom().run {
+            return@atBot
+        }
         try {
             HttpClient(CIO).use { httpClient ->
                 httpClient.post<String>(
@@ -41,15 +56,7 @@ fun GroupMessageSubscribersBuilder.matchChatterBot() {
         }?.let { response ->
             reply(getMessageChainFromCQMessages(CQMessage(response).toList()))
             atStatusDao.addOrUpdateAtStatusAsync(AtStatus(sender.id, group.id, 0)).await()
-        }?:atStatusDao.findAtStatusByUserIdAndGroupIdAsync(sender.id, group.id).await().also { status ->
-            val replies = arrayOf(
-                    getNetImageStreamFromUrl(CALL_BOT_1).uploadAsImage() + "叫吾辈有什么事吗？主人～",
-                    getNetImageStreamFromUrl(CALL_BOT_2).uploadAsImage() + "诶？抱歉主人，刚才在稍微想点事情，叫吾辈有什么事吗？",
-                    getNetImageStreamFromUrl(CALL_BOT_3).uploadAsImage() + "唔姆？？"
-            )
-            reply(replies.random())
-            atStatusDao.addOrUpdateAtStatusAsync(status.copy(atCount = status.atCount + 1)).await()
-        }
+        }?:replyRandom()
 
     }
 }
